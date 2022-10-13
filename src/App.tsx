f803399c01a66b1209aba623036beaed99d8ffd5
xclarissa <clarissaxr@gmail.com>
2022-10-13 17:50:51 -0300

    Criacao de tema, componente e storybook

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+export function App() { 
+
+  return (
+    <>
+      <h1 className='font-bold text-2xl text-violet-500'> Hello </h1>
+      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-gray-600 hover:bg-cyan-300'>Entrar</button>
+    </>
+  )
+}
+ 
